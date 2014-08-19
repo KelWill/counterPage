7cd7f9a2fea528070891c763fc19f1de0ed0d150
= <will.keleher@gmail.com>
2014-08-18 17:40:50 -0700

    Initial Counter Commit

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+var makeCounter = function(id, config){
+    config = config || {};
+    var counterConfig = {
+        initial: '0:00.0',
+        direction: 'up',
+        format: '9999',
+        interval: '1',
+    };
+    for (var key in config){
+        counterConfig[key] = config[key];
+    }
+
+    counterConfig.id = id;
+
+    return counterConfig;
+};
+
+$(document).on('ready', function(){
+    var counters = [
+        makeCounter('counter1'),
+        makeCounter('counter2'),
+        makeCounter('counter3')
+    ];
+
+    for (var i = 0; i < counters.length; i++){
+        var c = counters[i];
+        $('#' + c.id).addClass('counter-analog').counter(c);
+    }
+});
